3f5c1cd0bf6bbb449af8493cae717a9e9e5ba160
josecrmrz <josecrmrz@gmail.com>
2016-08-02 14:39:22 -0500

    Publish version 1.0.0.8 with design change

--- CountdownTimer/Form1.Designer.cs
+++ CountdownTimer/Form1.Designer.cs
@@ -42,10 +42,11 @@
             // lblTimer
             // 
             this.lblTimer.AutoSize = true;
-            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTimer.Location = new System.Drawing.Point(81, 106);
+            this.lblTimer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(0, 0);
             this.lblTimer.Name = "lblTimer";
-            this.lblTimer.Size = new System.Drawing.Size(103, 29);
+            this.lblTimer.Size = new System.Drawing.Size(417, 108);
             this.lblTimer.TabIndex = 0;
             this.lblTimer.Text = "00:00:00";
             // 
@@ -53,7 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(421, 109);
             this.Controls.Add(this.lblTimer);
             this.Name = "TimerForm";
             this.Text = "00:00:00";
