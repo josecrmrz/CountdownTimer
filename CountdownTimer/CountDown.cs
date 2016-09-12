4d9e58a29bd3f19c23dd3d0045fb96ea71e02bc5
josecrmrz <josecrmrz@gmail.com>
2016-09-12 16:18:32 -0500

    Add countdown to 4:30 pm. Reset on double click

---
+++ CountdownTimer/CountDown.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CountdownTimer
+{
+    public partial class TimerForm : Form
+    {
+        public TimerForm()
+        {
+            InitializeComponent();
+            timerCountDown.Start();
+        }
+
+        private void timerCountDown_Tick_1(object sender, EventArgs e)
+        {
+            DateTime original = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 16, 30, 0);
+            DateTime updated = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+            TimeSpan timeleft = original.Subtract(updated);
+            lblTimer.Text = timeleft.ToString();
+            this.Text = lblTimer.Text;
+            if (timeleft.ToString() == "00:01:00")
+                lblTimer.ForeColor = Color.Red;
+            if (timeleft.ToString() == "00:00:00")
+                timerCountDown.Stop();
+        }
+
+        private void timerLabel_DoubleClick(object sender, EventArgs e)
+        {
+            timerCountDown.Start();
+        }
+
+    }
+}
